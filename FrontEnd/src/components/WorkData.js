0ba2e7773041caf452adec8f8f763c40b687e2e5
Artur0705 <artlil420@gmail.com>
2022-10-07 18:12:23 +1100

    Added a project data to use in project page

---
+++ FrontEnd/src/components/WorkData.js
@@ -0,0 +1,62 @@
+import project1 from "../assets/FridgeMagician.jpeg";
+import project2 from "../assets/J.A.T.E.jpeg";
+import project3 from "../assets/TechBlog.jpeg";
+import project4 from "../assets/NoteTaker.jpeg";
+import project5 from "../assets/Top10M&L.jpeg";
+import project6 from "../assets/WeatherDashboard.jpeg";
+import project7 from "../assets/WorkDayScheduler.jpeg";
+
+const ProjectData = [
+  {
+    imgSrc: project1,
+    title: "Fridge Magician",
+    text: "A website that allows a user to log in and update the contents of their fridge, then resenting a selection of recipes ordered based on what they have at home",
+    liveView: "https://fridge-magician.herokuapp.com",
+    github:
+      "https://github.com/Artur0705/fridge-magican?organization=Artur0705&organization=Artur0705",
+  },
+  {
+    imgSrc: project2,
+    title: "J.A.T.E Just Another Text Editor",
+    text: "A text editor that meets the PWA criteria and feature a number of data persistence techniques that serve as redundancy in case of the options is not supported by the browser. The application will also function offline.",
+    liveView: "https://just-text-editor.herokuapp.com",
+    github: "https://github.com/Artur0705/Text-Editor-PWA",
+  },
+  {
+    imgSrc: project3,
+    title: "Tech Blog",
+    text: "Tech Blog website where developers can publish their posts and comment on each others posts.        ",
+    liveView: "https://fridge-magician.herokuapp.com",
+    github: "https://github.com/Artur0705/Tech-Blog",
+  },
+  {
+    imgSrc: project4,
+    title: "Note Taker App",
+    text: "Application that allows users to write and save notes so they can keep track of tasks that needs to be completed. Once the task is completed then users can delete it.",
+    liveView: "https://notee-taker.herokuapp.com",
+    github: "https://github.com/Artur0705/Note-Taker",
+  },
+  {
+    imgSrc: project5,
+    title: "Top10M&L",
+    text: "This application will give you the top ten music tracks on various charts. From this list, you'll be able to view and/or hear the songs and also get the lyrics for each, plus some other interesting information",
+    liveView: "https://rschragger.github.io/MAR",
+    github: "https://github.com/Artur0705/MAR-1",
+  },
+  {
+    imgSrc: project6,
+    title: "Weather Dashboard",
+    text: "A weather dashboard which gives user ability to search wheather and see 5 days forecast for different cities.",
+    liveView: "https://artur0705.github.io/Weather-Dashboard",
+    github: "https://github.com/Artur0705/Weather-Dashboard",
+  },
+  {
+    imgSrc: project7,
+    title: "Work Day Scheduler",
+    text: "A day planner which gives the user ability to create and save events.",
+    liveView: "https://artur0705.github.io/Work-Day-Scheduler",
+    github: "https://github.com/Artur0705/Work-Day-Scheduler",
+  },
+];
+
+export default ProjectData;
