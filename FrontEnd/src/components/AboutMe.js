d86686526980040ebfd9347eb8b3e20e64066f4c
Artur0705 <artlil420@gmail.com>
2022-10-07 20:46:12 +1100

    Updated css styles

--- FrontEnd/src/components/AboutMe.js
+++ FrontEnd/src/components/AboutMe.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./AboutMeStyles.css";
 import { Link } from "react-router-dom";
-
+import resume from "../assets/artur-resume.pdf";
 import myPhoto2 from "../assets/myphoto2.jpg";
 import myPhoto1 from "../assets/myphoto1.jpg";
 
@@ -10,10 +10,21 @@ const AboutMe = () => {
     <div className="about">
       <div className="left">
         <h1>Hello</h1>
-        <p>Test text</p>
-        <Link to="/contact">
-          <button className="btn">Contact Me</button>
-        </Link>
+        <p className="about-me-text">
+          This portfolio is showcasing my skills and you can find some of my
+          recent works in the Project section.Feel free to browse my portfolio
+          and if you have any questions please contact me via the form provided
+          by clicking the button below, also you can see my CV by clicking the
+          CV button.
+        </p>
+        <div className="about-btns">
+          <Link to="/contact">
+            <button className="about-btn btn">Contact</button>
+          </Link>
+          <Link to={resume} target="_blank">
+            <button className="about-btn btn">CV</button>
+          </Link>
+        </div>
       </div>
 
       <div className="right">
--- FrontEnd/src/components/Main.js
+++ FrontEnd/src/components/Main.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Typical from "react-typical";
 import "./Mainstyles.css";
 
 const Main = (props) => {
@@ -6,7 +7,7 @@ const Main = (props) => {
     <div className="main">
       <div className="heading">
         <h1>{props.heading}</h1>
-        <p>{props.text}</p>
+        <Typical className="about-text" loop={1} steps={props.text} />
       </div>
     </div>
   );
