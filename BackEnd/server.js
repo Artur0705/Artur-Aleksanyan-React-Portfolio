d98ce9948ca5c76f9b45e496e8cfc6435e6690e2
Artur0705 <artlil420@gmail.com>
2022-10-08 17:06:27 +1100

    Pushing to Heroku

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = require("express").Router();
 require("dotenv").config();
 const cors = require("cors");
+const path = require('path');
+
 const { sendEmail } = require("./SendEmail");
 
 const app = express();
@@ -11,6 +13,14 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+});
 
 const PORT = process.env.PORT || 8080;
 
