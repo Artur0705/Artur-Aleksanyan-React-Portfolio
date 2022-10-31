9b4349d687da133b9c3e5eca43e0fa1f5a55c800
Artur0705 <artlil420@gmail.com>
2022-10-31 19:54:56 +1100

    Added project to project page

--- FrontEnd/src/components/WorkData.js
+++ FrontEnd/src/components/WorkData.js
@@ -1,14 +1,22 @@
-import project1 from "../assets/FridgeMagician.jpeg";
-import project2 from "../assets/J.A.T.E.jpeg";
-import project3 from "../assets/TechBlog.jpeg";
-import project4 from "../assets/NoteTaker.jpeg";
-import project5 from "../assets/Top10M&L.jpeg";
-import project6 from "../assets/WeatherDashboard.jpeg";
-import project7 from "../assets/WorkDayScheduler.jpeg";
+import project1 from "../assets/shoeson.jpeg";
+import project2 from "../assets/FridgeMagician.jpeg";
+import project3 from "../assets/J.A.T.E.jpeg";
+import project4 from "../assets/TechBlog.jpeg";
+import project5 from "../assets/NoteTaker.jpeg";
+import project6 from "../assets/Top10M&L.jpeg";
+import project7 from "../assets/WeatherDashboard.jpeg";
+import project8 from "../assets/WorkDayScheduler.jpeg";
 
 const ProjectData = [
   {
     imgSrc: project1,
+    title: "Shoes On !",
+    text: "A webpage that allows customers to log in, browse for their favourite footwear, add them to their basket, and securely purchase. ",
+    liveView: "https://shoes-on.herokuapp.com",
+    github: "https://github.com/Artur0705/ShoesOn",
+  },
+  {
+    imgSrc: project2,
     title: "Fridge Magician",
     text: "A website that allows a user to log in and update the contents of their fridge, then resenting a selection of recipes ordered based on what they have at home",
     liveView: "https://fridge-magician.herokuapp.com",
@@ -16,42 +24,42 @@ const ProjectData = [
       "https://github.com/Artur0705/fridge-magican?organization=Artur0705&organization=Artur0705",
   },
   {
-    imgSrc: project2,
+    imgSrc: project3,
     title: "J.A.T.E",
     text: "A text editor that meets the PWA criteria and feature a number of data persistence techniques that serve as redundancy in case of the options is not supported by the browser. The application will also function offline.",
     liveView: "https://just-text-editor.herokuapp.com",
     github: "https://github.com/Artur0705/Text-Editor-PWA",
   },
   {
-    imgSrc: project3,
+    imgSrc: project4,
     title: "Tech Blog",
     text: "Tech Blog website where developers can publish their posts and comment on each others posts.        ",
     liveView: "https://fridge-magician.herokuapp.com",
     github: "https://github.com/Artur0705/Tech-Blog",
   },
   {
-    imgSrc: project4,
+    imgSrc: project5,
     title: "Note Taker App",
     text: "Application that allows users to write and save notes so they can keep track of tasks that needs to be completed. Once the task is completed then users can delete it.",
     liveView: "https://notee-taker.herokuapp.com",
     github: "https://github.com/Artur0705/Note-Taker",
   },
   {
-    imgSrc: project5,
+    imgSrc: project6,
     title: "Top10M&L",
     text: "This application will give you the top ten music tracks on various charts. From this list, you'll be able to view and/or hear the songs and also get the lyrics for each, plus some other interesting information",
     liveView: "https://rschragger.github.io/MAR",
     github: "https://github.com/Artur0705/MAR-1",
   },
   {
-    imgSrc: project6,
+    imgSrc: project7,
     title: "Weather Dashboard",
     text: "A weather dashboard which gives user ability to search wheather and see 5 days forecast for different cities.",
     liveView: "https://artur0705.github.io/Weather-Dashboard",
     github: "https://github.com/Artur0705/Weather-Dashboard",
   },
   {
-    imgSrc: project7,
+    imgSrc: project8,
     title: "Work Day Scheduler",
     text: "A day planner which gives the user ability to create and save events.",
     liveView: "https://artur0705.github.io/Work-Day-Scheduler",
